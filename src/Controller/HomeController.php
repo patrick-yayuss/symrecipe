86bb07b2a7c29bf66a58f61bb4ffcd5acc45226a
unknown <kassip55@gmail.com>
2023-11-13 18:53:39 +0000

    premier commit du projet

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', 'home.index', methods:['Get'])]
+    function index() : Response {
+        return $this->render("home.html.twig");
+    }
+}
